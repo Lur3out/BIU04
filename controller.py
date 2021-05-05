86791ab2585418d0e4d084b2e3a6ea1708972f0e
Lur3out <aupovaleksandr@gmail.com>
2021-05-06 01:45:30 +0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -2,6 +2,8 @@ from led import LED
 from command import Command
 from enums import Color, CommandType
 
-
+"""
+  Simple example
+"""
 led = LED("/dev/robot/rgb/rgb1", "rgb1")
 led.setColor(Color.Blue)
